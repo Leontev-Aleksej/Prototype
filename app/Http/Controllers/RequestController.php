ef2701e5b11861ce83d3ad26cee0def02e4c4182
Leontev-Aleksej <leontevaleksej485@gmail.com>
2025-02-04 23:15:44 +0300

    1 step

---
+++ app/Http/Controllers/RequestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Requestion;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+
+class RequestController extends Controller
+{
+    
+}
---
+++ app/Models/Requestion.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Requestion extends Model
+{
+    use HasFactory;
+}
